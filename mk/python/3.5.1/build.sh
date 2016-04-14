9481c5a83067a57ad7e6ab78b9bc5da6d80b5fd5
sceext2 <sceext@foxmail.com>
2016-04-14 22:33:04 +0800

    commit after compile python3.5.1, the second time, not --enable-shared, add --pie, arm32bit

--- mk/python/3.5.1/build.sh
+++ mk/python/3.5.1/build.sh
@@ -21,7 +21,12 @@ patch -p1  < "${FILESDIR}/${PACKAGE}-android-locale.patch" || exit 1
 patch -Ep1 < "${FILESDIR}/${PACKAGE}-android-libmpdec.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-android-misc.patch" || exit 1
 
-./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --enable-shared --without-ensurepip || exit 1
+./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --without-ensurepip || exit 1
+
+# pause before actually make
+echo " ---> pause BEFORE start make "
+bash
+
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" install || exit 1
 
